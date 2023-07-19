c3d432ca27e594cd1769779276c2cd7a8295d014
Joe Kimmel <jkimmel@vmware.com>
2023-07-18 17:33:47 -0700

    008 - build command
    Signed-off-by: Joe Kimmel <jkimmel@vmware.com>

---
+++ 008.sh
@@ -0,0 +1,1 @@
+$CNB_LIFECYCLE_PATH/builder -log-level debug -layers="./layers" -group="./layers/group.toml" -analyzed="./layers/analyzed.toml" -plan="./layers/plan.toml" -buildpacks="./buildpacks" -app="./workspace" -platform="./platform"
