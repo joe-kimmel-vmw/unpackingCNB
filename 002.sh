5815028424e7fcd92cb6ceecfa117a6e590806fc
Joe Kimmel <jkimmel@vmware.com>
2023-07-24 16:59:38 -0700

    note about sourcing the 002.sh script
    Signed-off-by: Joe Kimmel <jkimmel@vmware.com>

--- 002.sh
+++ 002.sh
@@ -1,3 +1,7 @@
+set -eux
+
+# these env vars will be the same across all phases so its helpful to export them
+# NOTE for the variables to persiset you may have to `source 002.sh` rather than execute it directly
 export CNB_USER_ID=1000
 export CNB_GROUP_ID=1000
 export CNB_PLATFORM_API=0.12
