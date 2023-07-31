7c609726fd07b64e5d87bc58661b95415e72b7d4
Joe Kimmel <jkimmel@vmware.com>
2023-07-31 14:43:26 -0700

    remove the set eux from 002
    Signed-off-by: Joe Kimmel <jkimmel@vmware.com>

--- 002.sh
+++ 002.sh
@@ -1,5 +1,3 @@
-set -eux
-
 # these env vars will be the same across all phases so its helpful to export them
 # NOTE for the variables to persiset you may have to `source 002.sh` rather than execute it directly
 export CNB_USER_ID=1000
