c33b0ac4d72b8085b9a28b7da3d9717dc0a0c03f
Joe Kimmel <jkimmel@vmware.com>
2023-07-31 14:42:30 -0700

    remove the set -eux from 002 script
    Signed-off-by: Joe Kimmel <jkimmel@vmware.com>

--- 002.sh
+++ 002.sh
@@ -1,7 +1,5 @@
-set -eux
-
 # these env vars will be the same across all phases so its helpful to export them
-# NOTE for the variables to persiset you may have to `source 002.sh` rather than execute it directly
+# NOTE for the variables to persist you likely need to `source 002.sh` rather than execute it directly
 export CNB_USER_ID=1000
 export CNB_GROUP_ID=1000
 export CNB_PLATFORM_API=0.12
